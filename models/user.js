5432c459c06f17890fc928d2dfa6333642f7b5f9
John Rogers <jkr32@txstate.edu>
2018-02-16 03:09:59 -0600

    Progression on Udemy class on node.js

--- models/user.js
+++ models/user.js
@@ -1,14 +1,14 @@
 var mongoose = require ('mongoose');
-var bcrypt = require ('bcrypt-node.js');
+var bcrypt = require ('bcryptjs');
 var Scheme = mongoose.Schema;
 
 // The user schema attributes / characteristics / fields
 var UserSchema = new mongoose.Schema ({
-  email: { type: String, unique; true, lowercase: true},
+  email: { type: String, unique: true, lowercase: true},
   password: String,
 
   profile: {
-    name { type: String, default: ''},
+    name: { type: String, default: ''},
     picture: { type: String, default: ''}
   },
 
@@ -26,7 +26,7 @@ UserSchema.pre ('save', function (next) {
   if (!user.isModified ('paswword')) return next ();
   bcrypt.genSalt (10, function (err, salt) {
     if (err) return next (err);
-    bcrypt.hash (user.password, salt, null, function (err, hash) {
+    bcrypt.hash (user.password, salt, function (err, hash) {
       if (err) return next (err);
       user.password = hash;
       next ();
@@ -37,4 +37,8 @@ UserSchema.pre ('save', function (next) {
 //compare password in the database and the one that the user types in
 UserSchema.methods.comparePassword = function (password) {
   return bcrypt.compareSync (password, this.password);
-}
+};
+
+
+//This exports so any file can use
+module.exports = mongoose.model ('User', UserSchema);
--- server.js
+++ server.js
@@ -1,20 +1,37 @@
 var express = require ('express');
 var morgan = require ('morgan');
+var mongoose = require ('mongoose');
+var bodyParser = require ('body-parser');
+
+var User = require ('./models/user');
 var app = express ();
+
+mongoose.connect ('mongodb://root:John030390@ds139138.mlab.com:39138/nodejsproject', function (err) {
+  if (err) {
+    console.log (err);
+  } else {
+    console.log ("Connected to the database");
+  }
+});
 
 //Middleware
 app.use (morgan ('dev'));
+app.use (bodyParser.json ());
+app.use (bodyParser.urlencoded ({ extended: true}));
 
+app.post ('/create-user', function (req, res, next) {
+  var user = new User ();
 
-//create home url
-app.get ('/', function (req, res) {
-  var name = "John"
-  res.json ("My name is " + name);
+  user.profile.name = req.body.name;
+  user.password = req.body.password;
+  user.email = req.body.email;
+
+  user.save (function (err) {
+    if (err) next (err);
+    res.json ('Successfully created a new user');
+  });
 });
 
-app.get ('/catname', function (re, res) {
-  res.json ('batman');
-});
 
 app.listen (3000, function (err) {
   if (err) throw err;
